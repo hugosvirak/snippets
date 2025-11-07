67f1e19ec5dd9dcf0b8e7bfb7104e2367214178d
Hugo Svirak <hugo@hugosvirak.com>
2025-11-07 19:13:16 +0100

    Less code

--- pixi.js-react-v8-room-decoration/src/components/Ui.tsx
+++ pixi.js-react-v8-room-decoration/src/components/Ui.tsx
@@ -1,31 +1,29 @@
-import { useAppContext } from "../AppContext";
+import { useAppContext, type ObjectType } from "../AppContext";
+
+const UI_LABELS: Record<ObjectType, string> = {
+  BED: "Create Bed",
+  FRIDGE: "Create Fridge",
+  BEDSIDE_TABLE: "Create Bedside Table",
+};
+
+const OBJECTS = Object.keys(UI_LABELS) as ObjectType[];
 
 export function Ui() {
   const appContext = useAppContext();
 
   return (
     <>
-      <button
-        onClick={() => {
-          appContext.createObject("FRIDGE");
-        }}
-      >
-        Create Fridge
-      </button>
-      <button
-        onClick={() => {
-          appContext.createObject("BED");
-        }}
-      >
-        Create Bed
-      </button>
-      <button
-        onClick={() => {
-          appContext.createObject("BEDSIDE_TABLE");
-        }}
-      >
-        Create Bedside Table
-      </button>
+      {OBJECTS.map((object: ObjectType) => {
+        return (
+          <button
+            onClick={() => {
+              appContext.createObject(object);
+            }}
+          >
+            {UI_LABELS[object]}
+          </button>
+        );
+      })}
     </>
   );
 }
