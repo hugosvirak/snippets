96504a975a73493ba9da1db7494ed5bf457dfb77
Hugo Svirak <hugo@hugosvirak.com>
2025-11-07 20:27:56 +0100

    Format

--- calling-a-function-that-calls-a-function-that/javascript/main.js
+++ calling-a-function-that-calls-a-function-that/javascript/main.js
@@ -1,9 +1,9 @@
 function F1() {
-    console.log("Hello World");
+  console.log("Hello World");
 }
 
 for (let i = 0; i < 5; i++) {
-    console.time("function");
-    F1();
-    console.timeEnd('function')
+  console.time("function");
+  F1();
+  console.timeEnd("function");
 }
