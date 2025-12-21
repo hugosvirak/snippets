4092f4f894764322600f807fd60910ad02c731a2
Hugo Svirak <hugo@hugosvirak.com>
2025-12-21 01:45:50 +0100

    Python

--- calling-a-function-that-calls-a-function-that/code-generator/python.py
+++ calling-a-function-that-calls-a-function-that/code-generator/python.py
@@ -1,10 +1,11 @@
-num_functions = 50
+num_functions = 1000
 functions_code = ""
 for i in range(1, num_functions + 1):
     if i < num_functions:
-        functions_code += f"def  F{i}() {{ F{i+1}(); }}\n"
+        functions_code += f"def F{i}():\n    F{i+1}()\n"
     else:
-        functions_code += f"def F{i}() {{ }}\n"
+        functions_code += f"def F{i}():\n    pass\n\n"
+
 
 with open('benchmark.py', 'w') as file:
     file.write(functions_code)
